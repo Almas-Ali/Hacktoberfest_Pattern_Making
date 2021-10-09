ba692a78ab45f76b89ec99079e1e9573bd787302
Haider Ali <haiderali3010@gmail.com>
2021-10-10 00:47:23 +0500

    Added inverted full pyramid pattern in javascript.

---
+++ Javascript/inverted-full-pyramid-pattern.js
@@ -0,0 +1,19 @@
+// variables
+let pyramidRows = 10; // number of row of pyramids.
+let pyramid = "";
+
+//loops
+for (let i = 0; i < pyramidRows; i++) {
+    
+    // adding spaces
+    for (let j = 0; j < i; j++) pyramid += " ";
+    
+    // adding star
+    for (let k = 0; k < (pyramidRows - i) * 2 - 1; k++) pyramid += "*";
+
+    // line break
+    pyramid += "\n";
+}
+
+// render in dom.
+document.write(`<pre>${pyramid}</pre>`);
