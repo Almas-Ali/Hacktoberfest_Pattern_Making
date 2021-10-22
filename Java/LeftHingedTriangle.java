42d3e437a5781caf86dcfb37725d51fe4f77b48a
Flux <91804289+Fluxyl@users.noreply.github.com>
2021-10-21 19:33:33 -0700

    Added LeftHingedTriangle
    use a row higher than 5-10 for good results.
    symbol can be anything.

---
+++ Java/LeftHingedTriangle.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class UltraPattern {
+	public static void main(String[] args) {
+		
+		System.out.println("rows: ");
+		Scanner scanner = new Scanner(System.in);
+		
+	    int rows = scanner.nextInt();
+		
+		System.out.println("symbol: ");
+		String strSymbol = scanner.next();
+		
+		int columns = rows - 1;
+		int i, j = 1;
+		for (j = 1; j <= rows; j++) {			
+			columns =- 1;
+			for (i = 1; i <= 4 * j - 4; i++) {  
+				System.out.print(strSymbol);
+			}
+			System.out.println("?");
+		}
+				  
+		columns = 1;  
+		for (j = 1; j <= rows - 1; j++) {  
+			for (i = 1; i <= columns; i++){  
+				System.out.print(strSymbol);  
+			}
+			columns =+ 1;  
+			for (i = 1; i <= 3 * (rows - j) - 1 + 23; i++){  
+				System.out.print(strSymbol);  
+			}  
+			System.out.println("!");
+		}
+	}
+}
+		
