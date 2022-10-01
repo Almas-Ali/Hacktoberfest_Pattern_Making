21d6f347e2c55a1427fc955dfd9dc9ba35c20c25
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-10-01 14:49:14 +0530

    Create messageBox.c

---
+++ C/messageBox.c
@@ -0,0 +1,12 @@
+// Try this code on DEV-C++ IDE
+
+write(1, "\n\n\n \t\t\t SATYAM TRIPATHI", strlen("\n\n\n \t\t\t HacktoberFest"));
+printf("\n\n\t\t\t");
+MessageBox(0, "INVALID OPTION ! please try again !", "Hello", MB_ICONWARNING);
+MessageBox(0, "INVALID OPTION ! please try again !", "Hello", MB_ICONQUESTION);
+MessageBox(0, "INVALID OPTION ! please try again !", "Hello", MB_ICONINFORMATION);
+MessageBox(0, "INVALID OPTION ! please try again !", "Hello", MB_ICONERROR);
+MessageBox(0, "INVALID OPTION ! please try again !", "Hello", 1);
+MessageBox(0, "INVALID OPTION ! please try again !", "Hello", 2);
+MessageBox(0, "INVALID OPTION ! please try again !", "Hello", 3);
+MessageBox(0, "INVALID OPTION ! please try again !", "Hello", 4);
