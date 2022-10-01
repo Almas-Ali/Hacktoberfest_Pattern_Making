6d7b43ecffd9fed02b2d2de557b74ec1df83ceff
Pankaj Singh Sirari <95204858+pankajsirari222@users.noreply.github.com>
2022-10-01 23:40:16 +0530

    Update josephusproblem.java

--- Java/Recursion/josephusproblem.java
+++ Java/Recursion/josephusproblem.java
@@ -1,3 +1,5 @@
+/*Question : 
+
 Given the total number of persons n and a number k which indicates that k-1 persons are skipped and kth person is killed in circle in a fixed direction.
 
 The task is to choose the safe place in the circle so that when you perform these operations starting from 1st place in the circle, you are the last one remaining and survive.
@@ -18,6 +20,7 @@ Output: 4
 Explanation: There are 5 persons so 
 skipping 2 person i.e 3rd person will 
 be killed. Thus the safe position is 4.
+*/
 
 
 import java.util.*;
