59a0a7919d2a2b75c9a7166eee22c0e9d3eb3e97
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-10-01 18:35:25 +0530

    Update messageBox.c

--- C/messageBox.c
+++ C/messageBox.c
@@ -1,12 +1,14 @@
 // Try this code on DEV-C++ IDE
 
-write(1, "\n\n\n \t\t\t SATYAM TRIPATHI", strlen("\n\n\n \t\t\t HacktoberFest"));
-printf("\n\n\t\t\t");
-MessageBox(0, "INVALID OPTION ! please try again !", "Hello", MB_ICONWARNING);
-MessageBox(0, "INVALID OPTION ! please try again !", "Hello", MB_ICONQUESTION);
-MessageBox(0, "INVALID OPTION ! please try again !", "Hello", MB_ICONINFORMATION);
-MessageBox(0, "INVALID OPTION ! please try again !", "Hello", MB_ICONERROR);
-MessageBox(0, "INVALID OPTION ! please try again !", "Hello", 1);
-MessageBox(0, "INVALID OPTION ! please try again !", "Hello", 2);
-MessageBox(0, "INVALID OPTION ! please try again !", "Hello", 3);
-MessageBox(0, "INVALID OPTION ! please try again !", "Hello", 4);
+#include <windows.h>
+int main()
+{
+    MessageBox(0, "Alert, invalid option. Please try again.", "Hello world", MB_ICONWARNING);
+    MessageBox(0, "Alert, invalid option. Please try again.", "Hello world", MB_ICONQUESTION);
+    MessageBox(0, "Alert, invalid option. Please try again.", "Hello world", MB_ICONINFORMATION);
+    MessageBox(0, "Alert, invalid option. Please try again.", "Hello world", MB_ICONERROR);
+    MessageBox(0, "Alert, invalid option. Please try again.", "Hello world", 1);
+    MessageBox(0, "Alert, invalid option. Please try again.", "Hello world", 2);
+    MessageBox(0, "Alert, invalid option. Please try again.", "Hello world", 3);
+    MessageBox(0, "Alert, invalid option. Please try again.", "Hello world", 4);
+}
