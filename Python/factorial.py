a187dbdad462829eb762f9dd9844e633023b6a69
Pratheek R <68503360+pratheek-raghunath@users.noreply.github.com>
2022-10-03 11:37:22 +0530

    Added contributor details

--- Python/factorial.py
+++ Python/factorial.py
@@ -8,3 +8,6 @@ def factorial_recursive(n):
     if n == 1 or n == 0:
         return 1
     return n * factorial_recursive(n-1)
+
+## Contributor : Pratheek Raghunath
+## Github Link : https://github.com/pratheek-raghunath
--- Python/palindrome.py
+++ Python/palindrome.py
@@ -2,3 +2,6 @@ if s == s[::-1]:
     print("palindrome string")
 else:
     print("Not a palindrome string.")
+
+## Contributor : Pratheek Raghunath
+## Github Link : https://github.com/pratheek-raghunath
