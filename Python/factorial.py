8c38eec5282bf44cefdbb4ca2244bbd7874773f4
Pratheek R <68503360+pratheek-raghunath@users.noreply.github.com>
2022-10-03 11:28:34 +0530

    added two python programs

---
+++ Python/factorial.py
@@ -0,0 +1,10 @@
+def factorial_iter(n):
+    product = 1
+    for i in range(n):
+        product = product * (i+1)
+    return product
+
+def factorial_recursive(n):
+    if n == 1 or n == 0:
+        return 1
+    return n * factorial_recursive(n-1)
---
+++ Python/palindrome.py
@@ -0,0 +1,4 @@
+if s == s[::-1]:
+    print("palindrome string")
+else:
+    print("Not a palindrome string.")
