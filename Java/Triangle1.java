911ddae8f259e29748b3e3a69641eec354b1f82d
ANSHIKA010 <sanshikas010@gmail.com>
2022-10-06 03:07:02 +0530

    added Triangle1 pattern

---
+++ Java/Triangle1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Triangle1{
+    public static void main(String[] args){
+        int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+            System.out.print("*"); 
+            }
+            System.out.println();
+        }
+    }
+}
